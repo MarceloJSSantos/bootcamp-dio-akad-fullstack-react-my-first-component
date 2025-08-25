28b85b57c77f38d775018b3a9eb82c2c0aa9a43f
MarceloJSSantos <marcelojssantos2012@gmail.com>
2025-08-25 11:29:03 -0300

    Finalizando a estrutura básica

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,22 +1,24 @@
 export function Profile(){
     return (
         <div>
-            <img src="src\assets\my-photo-profile.jpeg" alt="Marcelo Santos" />
+            <img src="src\assets\my-photo-profile.jpeg"
+             alt="Marcelo Santos"
+             width="300px" />
             <h3>Marcelo Santos</h3>
-            <p>Engenheiro de Software</p>
+            <p><b>Último/Atual cargo: </b>Engenheiro de Software</p>
         </div>
     )
 }
 
 export function Job(){
     return(
-        <p>Atualmente não estou empregado.</p>
+        <p>Atualmente não estou empregado. Mas minha última colocação foi na NTT Data.</p>
     )
 }
 
 const JobFC: React.FC = () => {
     return(
-        <p>Atualmente não estou empregado (Function Component).</p>
+        <p>Estou atualmente apredendo React para atuação no Front End. <b>(Function Component)</b></p>
     )
 }
 
